6d39c1cdca79d42c18da688d4d54a68035aca385
никола белоусько <belnv@yandex.ru>
2023-02-24 17:54:49 +0300

    Финал

--- cats/serializers.py
+++ cats/serializers.py
@@ -13,6 +13,7 @@ from .models import Achievement, AchievementCat, Cat
 class Hex2NameColor(serializers.Field):
     def to_representation(self, value):
         return value
+
     def to_internal_value(self, data):
         try:
             data = webcolors.hex_to_name(data)
